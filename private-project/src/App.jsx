7d447b37b1f4ec736ee200d938d4310cbf040e71
Roverfloats <roverfloats@gmail.com>
2026-01-14 11:29:15 +0100

    first page

--- private-project/src/App.jsx
+++ private-project/src/App.jsx
@@ -1,8 +1,11 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<></>} />
+        <Route path="/" element={<Login/>} />
       </Routes>
     </BrowserRouter>
   )
