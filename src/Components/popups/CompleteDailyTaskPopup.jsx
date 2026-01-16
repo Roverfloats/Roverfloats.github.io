9590592cd056605fad2a5a00d7ff844354d5bb1b
TygoLiebers <roverfloats@gmail.com>
2026-01-16 19:29:30 +0100

    phone fix

--- src/Components/popups/CompleteDailyTaskPopup.jsx
+++ src/Components/popups/CompleteDailyTaskPopup.jsx
@@ -9,11 +9,11 @@ function CompleteDailyTaskPopup({setReload, setPopup, taskData}) {
     }
 
     return (
-        <div className="w-[400px]">
+        <div className="flex flex-col w-[200px] md:w-[400px]">
             <p className="text-[25px] text-center">Is this task complete?</p>
-            <div className="flex justify-between mt-[20px]">
+            <div className="flex w-full flex-col md:flex-row md:justify-between items-center mt-[20px]">
                 <button className="w-[150px] h-[40px] border-2 border-[#D0D0D0] bg-white rounded-[15px]" onClick={() => setPopup(false)}>No</button>
-                <button className="w-[150px] h-[40px] bg-[#0096FF] text-white rounded-[15px]" onClick={() => HandleComplete()}>Yes</button>
+                <button className="w-[150px] h-[40px] bg-[#0096FF] text-white rounded-[15px] mt-[20px] md:mt-[0]" onClick={() => HandleComplete()}>Yes</button>
             </div>
         </div>
     )
--- src/Components/popups/DeleteDailyTaskPopup.jsx
+++ src/Components/popups/DeleteDailyTaskPopup.jsx
@@ -9,11 +9,11 @@ function DeleteDailyTaskPopup({setReload, setPopup, taskData}) {
     }
 
     return (
-        <div className="w-[400px]">
+        <div className="flex flex-col w-[200px] md:w-[400px]">
             <p className="text-[25px] text-center">Are you sure you wanna delete this task?</p>
-            <div className="flex justify-between mt-[20px]">
+            <div className="flex w-full flex-col md:flex-row md:justify-between items-center mt-[20px]">
                 <button className="w-[150px] h-[40px] bg-[#0096FF] text-white rounded-[15px]" onClick={() => setPopup(false)}>No</button>
-                <button className="w-[150px] h-[40px] border-2 border-[#D0D0D0] bg-white rounded-[15px]" onClick={() => HandleDelete()}>Yes</button>
+                <button className="w-[150px] h-[40px] border-2 border-[#D0D0D0] bg-white rounded-[15px] mt-[20px] md:mt-[0]" onClick={() => HandleDelete()}>Yes</button>
             </div>
         </div>
 
